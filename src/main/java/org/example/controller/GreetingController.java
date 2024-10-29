108dcd0f711af949c2425524c53c8277543a514a
Ksaiko <legostory333@gmail.com>
2024-10-29 13:10:21 +0300

    add main html and change controller

--- src/main/java/org/example/controller/GreetingController.java
+++ src/main/java/org/example/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package org.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,6 +10,12 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting() {
         return "greeting";
+    }
+
+    @GetMapping
+    public String main (Model model) {
+        model.addAttribute("some", "Hello, Kostia");
+        return "main";
     }
 
 }
